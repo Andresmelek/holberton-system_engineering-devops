f666b0e6260242904b581664533a530e899094b1
Andresmelek <candres.isaza@gmail.com>
2020-02-24 18:54:40 -0500

    Shorting algorythm

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:([\WA-Za-z0-9]{1,13})\]/).join + "," +
-ARGV[0].scan(/to:([\WA-Za-z0-9]{1,13})\]/).join + ","+
-ARGV[0].scan(/flags:([\WA-Za-z0-9]{1,13})\]/).join
+puts ARGV[0].scan(/(?<=from:|to:|flag:)([A-Za-z0-9.+-:]{0,9}+)\b]/).join + ","
