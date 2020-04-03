33165fe95dd6f2b4bb78f7d5d1ba958c0b27650d
Andresmelek <candres.isaza@gmail.com>
2020-04-03 17:39:14 -0500

    fixing task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 #same as 0 but with puppet
 exec { 'add_header':
-  command  => "apt-get update && apt-get -y install nginx && sed -i '/listen 80 default_server;/a && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
+  command  => "apt-get update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-available/default && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
   provider => 'shell',
 }
