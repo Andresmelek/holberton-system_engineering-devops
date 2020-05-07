c868c42eb76d9f413221663e7b61fa2330e1a962
andresmelek <candres.isaza@gmail.com>
2020-05-07 12:51:38 -0500

    hot tiitles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,7 +10,7 @@ import requests
 def recurse(subreddit, hot_list=[], after=""):
     """ curssion """
     if after is None:
-        return title_list
+        return hot_list
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'linux:com.myapp:v.1'}
@@ -25,4 +25,4 @@ def recurse(subreddit, hot_list=[], after=""):
     for post in response.get('data').get('children'):
         title_list.append(post.get('data').get('title'))
     nextt = response.get('data').get('after')
-    return recurse(subreddit, title_list, nextt)
+    return recurse(subreddit, hot_list, nextt)
